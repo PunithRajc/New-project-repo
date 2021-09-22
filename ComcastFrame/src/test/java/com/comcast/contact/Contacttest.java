b6a969ca4dffefbc158cc02c8eca3ccb69101286
PunithRajc <c.puneethraj@gmail.com>
2021-09-22 12:43:12 +0530

    Pushing my new project

---
+++ ComcastFrame/src/test/java/com/comcast/contact/Contacttest.java
@@ -0,0 +1,22 @@
+package com.comcast.contact;
+
+import org.testng.annotations.Test;
+
+public class Contacttest {
+	@Test
+	public void createcontactTest() {
+		System.out.println("execute createContactTest");
+	}
+	
+	@Test
+	public void createcontactwithTest() {
+		System.out.println("execute createContactwithTest");
+	}
+	
+	
+	@Test
+	public void deletecreatecontactTest() {
+		System.out.println("execute deletecreateContactTest");
+	}
+
+}
